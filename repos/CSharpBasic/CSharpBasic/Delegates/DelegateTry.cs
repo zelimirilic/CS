04bdf5f88c11f9a6bfa1fe68e3ab4a911c94aca7
Želimir Ilić <zelimir.ilic@consolia.rs>
2019-08-29 10:17:50 +0200

    Testing Automation THE END

--- repos/CSharpBasic/CSharpBasic/Delegates/DelegateTry.cs
+++ repos/CSharpBasic/CSharpBasic/Delegates/DelegateTry.cs
@@ -7,14 +7,20 @@ using System.Threading.Tasks;
 namespace CSharpBasic.Delegates
 {
     //Declaration delegates
-    delegate void Printer(string printValue);
-    class DelegateTry
+    //delegate void Printer(string printValue);
+    public class DelegateTry
     {
+
+        
         public static void PrintValue(string values)
         {
-            Console.WriteLine($"The value is {values}.");
+            Console.WriteLine($"The value is: {values}.");
         }
-        
-        
+        public static void PrintValue1(string values)
+        {
+            Console.WriteLine($"The value from printvalue1 is: {values}.");
+        }
+
+
     }
 }
--- repos/CSharpBasic/CSharpBasic/Program.cs
+++ repos/CSharpBasic/CSharpBasic/Program.cs
@@ -2,6 +2,7 @@ using System;
 using CSharpBasic.Browsers;
 using CSharpBasic.Selenium;
 using CSharpBasic.ExstensionMethod;
+using CSharpBasic.Delegates;
 
 namespace CSharpBasic
 {
@@ -23,7 +24,41 @@ namespace CSharpBasic
             //IWebDriver driver = new Chrome();
             //driver.SendKeyWithSpecChar("Text about automation testing", "!`~~`!");
 
-            CollectionExample.GenericCollectionWithCustomClass();
+            //CollectionExample.GenericCollectionWithCustomClass();
+
+            // delegates example
+            //C# 1.0
+            //Printer p = DelegateTry.PrintValue1;
+            //p("Executing automation test.....");
+
+            //C# 2.0
+            //Printer p = delegate (string values)
+            //{
+            //    Console.WriteLine($"The value printed inline {values}");
+            //};
+
+            //p("Executing automation test.....");
+
+            //C# 3.0
+            //Printer p = (values) => { Console.WriteLine($"The value printed from Lambda Expressions is: {values}"); };
+            //p("Executing AT again.....");
+
+            Func<string, string> p = delegate(string values)
+            {
+                return values;
+            };
+
+            Console.WriteLine($"The value from Func<> is ovo! {p("Surprise !!!")}");
+
+            Action<string> pp = delegate (string values)
+            {
+                Console.WriteLine($"The value from Func<> is this {values}");
+            };
+            pp("New surprice !!!");
+
+            Action<string> pLambda = values => { Console.WriteLine($"The value from action Lambda is {values}"); };
+            pLambda("Excellent surprise !!!");
+
 
             Console.ReadLine();
             
